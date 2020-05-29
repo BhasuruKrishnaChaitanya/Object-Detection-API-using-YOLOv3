2dd133330d237339236ca09c084a6b66e6603274
BhasuruKrishnaChaitanya <48602330+BhasuruKrishnaChaitanya@users.noreply.github.com>
2020-05-29 15:43:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ weights_path = './weights/yolov3.tf'
 tiny = False                    # set to True if using a Yolov3 Tiny model
 size = 416                      # size images are resized to for model
 output_path = './detections/'   # path to output folder where images with detections are saved
-num_classes = 80                # number of classes in model
+num_classes = 3                # number of classes in model
 
 # load in weights and classes
 physical_devices = tf.config.experimental.list_physical_devices('GPU')
